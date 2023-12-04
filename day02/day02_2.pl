f50afcf359b46cfd4a9f0d5566ca154178f65462
Johannes <johannes.fleck@gmail.com>
2023-12-04 21:29:23 +0100

    Update day02_2.pl

--- day02/day02_2.pl
+++ day02/day02_2.pl
@@ -1,15 +1,15 @@
 use strict;use warnings;
 our $gameSum = 0;
 open (FH, "./data.txt") or die "Cannot open file $!";
-while (<FH>){
+while (<FH>){  
   my $redMax = 0; my $greenMax = 0; my $blueMax = 0;
   foreach (split(/[,;]/, substr $_,(index $_,":")+1)) {
-    $_ =~ s/^\s+//;
+    $_ =~ s/^\s+//; 
     my @ball = split (" ", $_);
-    if ($ball[1] eq "red") { if ($ball[0] > $redMax) { $redMax = $ball[0]; } }
-    if ($ball[1] eq "green") { if ($ball[0] > $greenMax) { $greenMax = $ball[0]; } }
-    if ($ball[1] eq "blue") { if ($ball[0] > $blueMax) { $blueMax = $ball[0]; } }
+    if (($ball[1] eq "red")   && ($ball[0] > $redMax))   { $redMax = $ball[0];   }  
+    if (($ball[1] eq "green") && ($ball[0] > $greenMax)) { $greenMax = $ball[0]; } 
+    if (($ball[1] eq "blue")  && ($ball[0] > $blueMax))  { $blueMax = $ball[0];  } 	
   }
-  $gameSum += ($redMax * $greenMax * $blueMax);
+  $gameSum += ($redMax * $greenMax * $blueMax); 
 }
 print "Solution:$gameSum\n";
